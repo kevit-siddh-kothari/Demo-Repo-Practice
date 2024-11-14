2b60ed53b955f93955d9a47f14f8df5585968ea6
siddh-kothari <siddh.kothari@kevit.io>
2024-11-14 23:02:04 +0530

    feat(model) : address model added

--- project-one/src/app/app.module.ts
+++ project-one/src/app/app.module.ts
@@ -4,19 +4,25 @@ import { AppService } from './app.service';
 import { UserModule } from '../componets/user/user.module';
 import { AddressModule } from '../componets/address/address.module';
 // import { GlobalModel } from 'src/global-model.ts/global-model.ts.module';
-import  {ConfigModule} from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConnectionMongodb } from 'src/db-connection/db-connection';
 import { GlobalModel } from 'src/global-model.ts/global-model.ts.module';
 
 @Module({
-  imports: [UserModule, AddressModule, GlobalModel, ConfigModule.forRoot({
-    cache:true,
-    isGlobal: true,
-    envFilePath: ['.env']
-  }), MongooseModule.forRootAsync({
-    useClass : MongooseConnectionMongodb
-  })],
+  imports: [
+    UserModule,
+    AddressModule,
+    GlobalModel,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongooseConnectionMongodb,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- project-one/src/componets/address/entities/address.entity.ts
+++ project-one/src/componets/address/entities/address.entity.ts
@@ -1,1 +1,16 @@
-export class Address {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { Collection } from 'src/global-constants/global.enum';
+
+@Schema({ timestamps: true, collection: Collection.ADDRESS })
+export class Address {
+  @Prop({ type: String })
+  state: string;
+
+  @Prop({ type: String })
+  city: string;
+}
+
+export type addressDocument = Document & Address;
+
+export const addressSchema = SchemaFactory.createForClass(Address);
